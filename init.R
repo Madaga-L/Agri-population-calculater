0e5b63ae56fdf338e18f1d3bdc2aa3f275cef3c6
Madaga-L <91804577+Madaga-L@users.noreply.github.com>
2023-05-21 18:10:22 +0300

    Add files via upload

--- init.R
+++ init.R
@@ -3,6 +3,8 @@
 # Example R code to install packages if not already installed
 #
 
+my_packages = c("sf", "dplyr", "rgdal", "terra", "raster", "leaflet", "leaflet.extras")
+
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
